474c2563270e443c1d0901fad24cfb3edf22dfc6
Renan Caldas <renan@renancaldas.dev.br>
2024-07-19 09:01:24 -0300

    section de faq

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(
+    ".the-best-service-for-you-accordion-button"
+  );
+
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+
+      if (content.style.display === "block") {
+        content.style.display = "none";
+        button.classList.remove("open");
+      } else {
+        document
+          .querySelectorAll(".the-best-service-for-you-accordion-content")
+          .forEach((item) => {
+            item.style.display = "none";
+          });
+        document
+          .querySelectorAll(".the-best-service-for-you-accordion-button")
+          .forEach((btn) => {
+            btn.classList.remove("open");
+          });
+        content.style.display = "block";
+        button.classList.add("open");
+      }
+    });
+  });
+});
