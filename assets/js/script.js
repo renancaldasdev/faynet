010df53d1044184c0dc47465a80bd0b4e3e1bd24
Renan Caldas <renan@renancaldas.dev.br>
2024-08-02 08:14:14 -0300

    adicionando carrousel de testemunhos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// plans
-
-// Accordion
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll(
     ".the-best-service-for-you-accordion-button"
@@ -29,4 +26,42 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+    .then((response) => response.json())
+    .then((photos) => {
+      const swiperWrapper = document.querySelector(".swiper-wrapper");
+      swiperWrapper.innerHTML = "";
+
+      photos.forEach((photo) => {
+        const slide = document.createElement("div");
+        slide.className = "swiper-slide";
+        slide.innerHTML = `
+        <img src="${photo.url}" alt="${photo.title}">
+               <p>${photo.title}</p>
+        `;
+        swiperWrapper.appendChild(slide);
+      });
+
+      const swiper = new Swiper(".swiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        effect: "fade",
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        coverFlow: {
+          depth: 100,
+        },
+      });
+    })
+    .catch((error) => console.error("Erro ao carregar fotos:", error));
 });
